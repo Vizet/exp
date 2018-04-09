38e10f287c27b3b0d9ee4b4c23ad494c8b8c9e2e
Roman <om_roman95@mail.ru>
2018-04-09 21:08:26 +0300

    testnet for binance(without db logs)

--- bot.js
+++ bot.js
@@ -1,9 +1,111 @@
+// binance bot
+
+var axios = require('axios')
+
 module.exports = class Bot{
-	constructor(){
-		return this
+
+    /*      TODO
+        * хранение состояния бота в БД
+        * загрузка состояния бота их БД
+    */
+
+	constructor({symbol, timeframe, buy, sell, startCandles}){
+
+	    this.symbol = symbol
+        this.timeframe = timeframe
+        this.buy = buy
+        this.sell = sell
+        this.positionOpen = false
+        this.candles = []
+
+        this.streamName = symbol.toLowerCase()+'@kline_'+timeframe // ltcusdt@kline_1h
+
+
+        axios.get('https://api.binance.com/api/v1/klines?symbol='+this.symbol.toUpperCase()+'&interval='+timeframe+'&limit='+startCandles)
+            .then(response => {
+                console.log('данные загружены в бота')
+                this.candles = response.data
+            })
+            .catch(e => console.log('Ошибка при загрузке стартовых данных', e))
+
 	}
 
-	sayRak(){
-		console.log('rak it')
-	}
+	get endLastCandleTime(){
+	    return this.candles[this.candles.length][6]
+    }
+
+
+    buyAsset(){
+
+    }
+
+    sellAsset(){
+
+    }
+
+    processNewCandle(newCandle){
+
+        if(this.candles.length > 0 && newCandle.k.t === this.candles[this.candles.length - 1][0]){
+            this.candles.pop()
+        }
+
+        if(newCandle.k.x){
+            this.candles.push(this.objCandleToArray(newCandle.k))
+
+            if(!this.positionOpen){
+                if(this.buy(this.candles)){
+                    this.makeDeal('buy')
+                    this.positionOpen = true
+                }
+            }
+            else{
+                if(this.sell(this.candles)){
+                    this.makeDeal('sell')
+                    this.positionOpen = false
+                }
+            }
+
+        }
+    }
+
+    objCandleToArray(objCandle) {
+        return [
+            objCandle.t,
+            objCandle.o,
+            objCandle.h,
+            objCandle.l,
+            objCandle.c,
+            objCandle.v,
+            objCandle.T,
+            objCandle.q,
+            objCandle.n,
+            objCandle.V,
+            objCandle.Q,
+
+        ]
+    }
+
+    makeDeal(action = 'none'){
+        axios.get('https://api.binance.com/api/v1/depth?symbol=LTCUSDT&limit=5')
+            .then(response => {
+
+                var price = {
+                    bid: response.data.bids[0][0], // цена покупки(на бирже)
+                    ask: response.data.asks[0][0]  // цена продажи(на бирже)
+                }
+
+                if(action === 'buy'){
+                    console.log(this.symbol, 'покупка по цене', price.ask)
+                }
+
+                if(action === 'sell'){
+                    console.log(this.symbol, 'продажа по цене', price.bid)
+                }
+
+                console.log('BEST', price)
+            })
+            .catch(e => console.log('makeDeal что то не так', e))
+
+    }
+
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var axios = require('axios')
-var io = require('socket.io').listen(80)
+var _ = require('lodash')
+
 const WebSocket = require('ws')
+var io = require('socket.io').listen(80)
 
 var mongoClient = require('mongodb').MongoClient
 var Bot = require('./bot.js')
@@ -9,117 +11,233 @@ var Bot = require('./bot.js')
 var app = express()
 
 var botState = {
-    positionStatus: 'none'
+    positionStatus: 'none',
+    priceInfo: {
+
+    }
 }
 
-var dbGlobal // что то нужно с этим сделать
+var DB
+
+var candleArr = []
+
+
+
+
+
 
 // юзаю сокеты для конекта к стриму свечей
 
-const ws = new WebSocket('wss://stream.binance.com:9443/ws/ltcusdt@kline_1m');
+
+
+function objCandleToArray(objCandle) {
+    return [
+        objCandle.t,
+        objCandle.o,
+        objCandle.h,
+        objCandle.l,
+        objCandle.c,
+        objCandle.v,
+        objCandle.T,
+        objCandle.q,
+        objCandle.n,
+        objCandle.V,
+        objCandle.Q,
+
+    ]
+}
+
 
 function makeDeal(action = 'none'){
     axios.get('https://api.binance.com/api/v1/depth?symbol=LTCUSDT&limit=5')
-    .then(response => {
+        .then(response => {
 
-        var price = {
-            bid: response.data.bids[0][0], // цена покупки(на бирже)
-            ask: response.data.asks[0][0]  // цена продажи(на бирже)
-        }
+            var price = {
+                bid: response.data.bids[0][0], // цена покупки(на бирже)
+                ask: response.data.asks[0][0]  // цена продажи(на бирже)
+            }
 
-        if(action == 'buy'){
-            console.log('покупка по цене', price.ask)
-        }
+            if(action === 'buy'){
+                console.log('фактическая цена открытия', price.ask)
+                botState.priceInfo.openPriceFact = price.ask
+                DB.db('admin').collection("bots").insert(botState.priceInfo, function(err, res){
+                    console.log('записали сделку в базу')
+                })
+            }
 
-        if(action == 'sell'){
-            console.log('продажа по цене', price.bid)
-        }
+            if(action === 'sell'){
+                console.log('фактическая цена закрытия', price.bid)
+                botState.priceInfo.clsoePriceFact = price.bid
 
-        console.log('BEST', price)
-    })
-    .catch(e => console.log('makeDeal что то не так', e))
+                // тут запись в базу
+
+                DB.db('admin').collection("bots").insert(botState.priceInfo, function(err, res){
+                    console.log('записали сделку в базу')
+                })
+            }
+
+            // console.log('BEST', price)
+            console.log('--------------------------------------------------')
+        })
+        .catch(e => console.log('makeDeal что то не так', e))
 
 }
 
-ws.on('message', function incoming(data) {
-    
-    objData = JSON.parse(data)
-    if(objData.k.x){
-        console.log('свеча закрыта', objData.k.x)
 
-        axios.get('https://api.binance.com/api/v1/klines?symbol=LTCUSDT&interval=1m&limit=3')
+
+function startBot(){
+    axios.get('https://api.binance.com/api/v1/time')
         .then(response => {
-            // логику принятия решений пишу пока здесь
-            var arr = response.data.map(el => el[4] > el[1] ? 'green' : 'red')
-            arr.pop() // удаляем последнюю свечу тк она текущая - те только открылась
-
-
-            if(botState.positionStatus == 'none'){
-                // позиция не открыта, ищем вход
-                const twoRedCandles = arr.every(el => el == 'red')
-                
-
-                if(twoRedCandles){
-                    // покупаем
-                    makeDeal('buy')
-
-                }else{
-                    console.log('нет условий для покупки', arr)
-                }
-                console.log(arr.every(el => el == 'red'))
-            }
-
-            if(botState.positionStatus = 'opened'){
-                const oneGreenCandle = arr[arr.length - 1] == 'green'
-
-                if(oneGreenCandle){
-                    // закрываем позицию(продаем)
-                    makeDeal('sell')
-
-                }else{
-                    console.log('еще не продаем')
-                }
-            }
-
-            console.log('получил весь чарт', arr)
-
+            console.log('time', Date.now() - response.data.serverTime)
         })
+        .catch(e => console.log(e))
 
-    }else{
-        console.log('открыта', objData.k.x)
+    axios.get('https://api.binance.com/api/v1/klines?symbol=LTCUSDT&interval=1h&limit=3')
+        .then(response => {
+            candleArr = response.data
+            // console.log('candleArr', candleArr)
+            // setInterval(checkCandleEndTime.bind(this), 1000)
+        })
+        .catch(e => console.log(e))
+}
+
+
+var botsCollection = {};
+
+function startBots(botsArray){
+    botsArray.forEach(currentBotOptions => {
+
+        let streamName = currentBotOptions.symbol.toLowerCase()+'@kline_'+currentBotOptions.timeframe // ltcusdt@kline_1h
+
+        botsCollection[streamName] = new Bot(currentBotOptions)
+    })
+
+
+    console.log('botsCollection', botsCollection)
+
+    let streamStr = Object.keys(botsCollection).join('/')
+    console.log('streamStr', streamStr)
+
+    const ws = new WebSocket('wss://stream.binance.com:9443/stream?streams='+streamStr);
+
+    ws.on('message', function (streamData) {
+        try{
+            let objData = JSON.parse(streamData)
+            botsCollection[objData.stream].processNewCandle(objData.data)
+        }
+        catch(e){
+            console.log('ошибка при обработке новых данных', e, streamData)
+        }
+
+    })
+
+}
+
+startBots([
+    {
+        symbol: 'ltcusdt',
+        timeframe: '1h',
+        buy: function (candles) {
+            let colorCandlesArr = candles.slice(candles.length - 2, candles.length).map(el => el[4] > el[1] ? 'green' : 'red')
+            let twoRedCandles = colorCandlesArr.every(el => el === 'red')
+            return twoRedCandles
+        },
+        sell: function (candles) {
+            let colorCandlesArr = candles.slice(candles.length - 2, candles.length).map(el => el[4] > el[1] ? 'green' : 'red')
+            let oneGreenCandle = colorCandlesArr[colorCandlesArr.length - 1] === 'green'
+            return oneGreenCandle
+        },
+        startCandles: 3
+    },
+    {
+        symbol: 'neousdt',
+        timeframe: '1h',
+        buy: function (candles) {
+            let colorCandlesArr = candles.slice(candles.length - 2, candles.length).map(el => el[4] > el[1] ? 'green' : 'red')
+            let twoRedCandles = colorCandlesArr.every(el => el === 'red')
+            return twoRedCandles
+        },
+        sell: function (candles) {
+            let colorCandlesArr = candles.slice(candles.length - 2, candles.length).map(el => el[4] > el[1] ? 'green' : 'red')
+            let oneGreenCandle = colorCandlesArr[colorCandlesArr.length - 1] === 'green'
+            return oneGreenCandle
+        },
+        startCandles: 3
     }
+])
 
-    // эта часть должна быть внтри бота
 
-    
-});
 
-mongoClient.connect("mongodb://localhost:27017/admin", function(err, db){
-    if(err){
-        return console.log('что-то пошло не так', err)
-    }
+// const ws = new WebSocket('wss://stream.binance.com:9443/ws/ltcusdt@kline_1h');
 
-    console.log('конект к db прошел')
 
-    // добавленеи документа в коллекцию
+// ws.on('message', function(data) {
+//
+//     let currentCandle = JSON.parse(data)
+//
+//     if(currentCandle.k.t === candleArr[candleArr.length - 1][0]){
+//         console.log('текущая свеча уже в массиве, удаляем')
+//         candleArr.pop()
+//     }
+//
+//     if(currentCandle.k.x){
+//         candleArr.push( objCandleToArray(currentCandle.k) )
+//
+//         // логику принятия решений пишу пока здесь
+//
+//         let colorCandlesArr = candleArr.slice(candleArr.length - 2, candleArr.length).map(el => el[4] > el[1] ? 'green' : 'red')
+//
+//         console.log('свеча закрылась, colorCandlesArr', colorCandlesArr)
+//
+//
+//         if(botState.positionStatus === 'none'){
+//             // позиция не открыта, ищем вход
+//             const twoRedCandles = colorCandlesArr.every(el => el === 'red')
+//
+//
+//             if(twoRedCandles){
+//                 // покупаем
+//                 makeDeal('buy')
+//                 console.log('цена открытия позиции', colorCandlesArr, currentCandle.k.o)
+//                 botState.positionStatus = 'opened'
+//                 botState.priceInfo.openPrice = currentCandle.k.o
+//
+//             }else{
+//                 // console.log('нет условий для покупки', colorCandlesArr)
+//             }
+//         }else if(botState.positionStatus === 'opened'){
+//             // уже купили, пытаемся продать
+//             const oneGreenCandle = colorCandlesArr[colorCandlesArr.length - 1] === 'green'
+//
+//             if(oneGreenCandle){
+//                 // закрываем позицию(продаем)
+//                 makeDeal('sell')
+//                 console.log('цена закрытия позиции', colorCandlesArr, currentCandle.k.c)
+//                 botState.priceInfo.closePrice = currentCandle.k.c
+//
+//                 botState.positionStatus = 'none'
+//
+//             }else{
+//                 // console.log('еще не продаем', colorCandlesArr)
+//             }
+//         }
+//
+//
+//
+//     }else{
+//         // console.log('открыта', objData.k.x)
+//     }
+//
+//     // эта часть должна быть внтри бота
+//
+//
+// });
 
-    // var collection = db.db('admin').collection("bots")
-    // var bot = {
-    //     name: 'my first bot1',
-    //     type: 'some type',
-    //     currency: 'LTCUSDT',
-    //     birzha: 'binance'
-    // }
 
-    // collection.insertOne(bot, function(err, res){
-    //     console.log('вроде как что то произошло')
-    // })
 
-    db.close()
-})
 
-var b = new Bot();
-b.sayRak()
+
+
 
 
 
@@ -132,25 +250,44 @@ app.use(function (req, res, next) {
 
 app.get('/', function (req, res) {
     console.log('user request')
-
-    axios.get('https://yobit.net/api/3/ticker/etc_usd')
-        .then(function (response) {
-            res.send(response.data)
-        })
-        .catch(function (err) {
-            res.send('Какая то ошибка')
-        })
 })
 
 
-io.on('connection', function (socket) {
-    console.log('socket connect')
+mongoClient.connect("mongodb://localhost:27017/admin", function(err, database){
+    if(err){
+        return console.log('ошибка при коннекте к монге', err)
+    }
 
-    socket.on('dnoevent', function (txt) {
-        console.log('txt', txt)
+    console.log('конект к db прошел')
+
+    DB = database
+
+    // добавленеи документа в коллекцию
+
+
+    app.listen('3000', function () {
+
+        // startBot()
+
+        var dno = {
+            a: 1,
+            b: 2
+        }
+
+        // DB.db('admin').collection("bots").insert(_.cloneDeep(dno) , function(err, res){
+        //     console.log('записали сделку в базу')
+        // })
+        //
+        // DB.db('admin').collection("bots").insert(_.cloneDeep(dno), function(err, res){
+        //     console.log('записали сделку в базу')
+        // })
+
+
     })
+
+    // botHistory.insertOne(bot, function(err, res){
+    //     console.log('вроде как что то произошло')
+    // })
+
 })
 
-app.listen('3000', function () {
-    console.log('started')
-})
